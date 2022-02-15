7eb611de89e85667fc2bd63e87b327d0f6745e9a
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-02-15 17:43:24 +0530

    Pangram or Not
    To check whether given String is a Pangram or not.

---
+++ pangram.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char str[10000];
+	int i,j,up=0,lw=0,len,ans=0;
+	scanf("%[^\n]s", str);
+	len=strlen(str);
+	if(len<26)
+	{
+		printf("No the given String is not a Panagram");
+	}
+	else
+	{
+		for(i=0;i<len;i++)
+		{
+			int count=0;
+			if(str[i]>='A' && str[i]<='Z')
+			{
+				str[i]+=32;
+			}
+			for(j=0;j<=i;j++)
+			{
+				if(str[i]==str[j] && i!=j)
+				{
+					count++;
+				}
+			}
+			if(str[i]>='a' && str[i]<='z' && count==0)
+			{
+				ans++;
+			}
+		}
+		if(ans==26)
+		{
+			printf("Yes the given String is a Panagram");
+		}
+		else
+		{
+			printf("No the given String is not a Panagram");
+		}
+	}
+}
